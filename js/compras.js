572c12e52decf297ba13524164c970d76c29391c
Rodnei3009 <rbrassoroto@yahoo.com.br>
2017-04-08 20:47:38 -0300

    xxxxx
    xxxx

--- js/compras.js
+++ js/compras.js
@@ -1,5 +1,42 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
+    
+    $scope.prod_incluir = "";
+    
+    $scope.categorias = [{
+      id: 1,
+      label: 'Alimentos'
+    }, {
+      id: 2,
+      label: 'Bebidas'
+    }];
+    
+    $scope.selected = $scope.categorias[0];
+    
+    $scope.produtos = [{
+      id: 1,
+      label: 'Arroz',
+      categ: 1       
+    }, {
+      id: 2,
+      label: 'Feijão',
+      categ: 1    
+    }, {
+      id: 3,
+      label: 'Cerveja',
+      categ: 2    
+    }, {
+      id: 4,
+      label: 'Refrigerante',
+      categ: 2    
+    }];
+    
+    $scope.saveProduct = function(desc, categ){
+        //alert(desc + " - " + categ);
+        if (desc != "") {
+            $scope.produtos.push({id:10, label: desc, categ: categ});
+            $scope.prod_incluir = "";
+        }    
+    };
+    
 });
